840bfef96ef4c6b1e95e6333a3afccb0713db033
ThieuKhang <165531664+ThieuKhang@users.noreply.github.com>
2024-12-10 14:56:50 +0700

    Add files via upload

---
+++ webEdu/src/main/java/dev/start/webEdu/WebEduApplication.java
@@ -0,0 +1,13 @@
+package dev.start.webEdu;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebEduApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WebEduApplication.class, args);
+	}
+
+}
---
+++ webEdu/src/test/java/dev/start/webEdu/WebEduApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.start.webEdu;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebEduApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
